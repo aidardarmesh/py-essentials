20f853dff47e52c276292e31b45c289aed164537
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-16 13:39:57 +0600

    Almost done. Need to implement add and some kind of context manager replacing open, because I can't close file caret at the end of file

--- week4/magic_methods.py
+++ week4/magic_methods.py
@@ -1,11 +1,43 @@
-class Sample:
-    def __setitem__(self, key, value):
-        print("trying set {}: {}".format(key, value))
-        super().__setitem__(self, key, value)
-    
-    def __getitem__(self, key):
-        print("trying get {}".format(key))
+import os.path, tempfile
 
-s = Sample()
-s['marko'] = 'polo'
-s['marko']
+class File:
+    """Интерфейс для работы с файлами"""
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def write(self, data):
+        with open(self.file_path, 'w') as f:
+            f.write(data)
+    
+    def __add__(self, obj):
+        
+
+    def __str__(self):
+        return self.file_path
+    
+    def __iter__(self):
+        return open(self.file_path, 'r')
+
+    def __next__(self):
+        line = self.readline()
+
+        if not line:
+            self.close()
+            print('I closed file')
+            raise StopIteration
+        
+        return line
+
+hello = File('hello.txt')
+world = File('world.txt')
+
+hello.write('hello\nmy\nfriend\n')
+print(hello)
+
+lines = []
+
+for line in hello:
+    lines.append(line)
+
+print(lines)
