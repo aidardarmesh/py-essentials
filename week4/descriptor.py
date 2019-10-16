8758215abe860d84053edd73d57728facb6a89f3
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-16 18:59:54 +0600

    Fixed typo

--- week4/descriptor.py
+++ week4/descriptor.py
@@ -1,12 +1,14 @@
 class Value:
+    """Дескриптор баланса с учетом комиссии"""
+
     def __get__(self, obj, obj_type):
         return self.value
     
     def __set__(self, obj, value):
-        self.value = int(value * (1 - obj.comission))
+        self.value = int(value * (1 - obj.commission))
 
 class Account:
     amount = Value()
 
     def __init__(self, comission):
-        self.comission = comission
+        self.commission = commission
