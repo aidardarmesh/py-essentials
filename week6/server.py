fc56ad9134559e420af77d2ce95dae22842e83c7
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-19 15:20:43 +0600

    Implemented starter function run_server

--- week6/server.py
+++ week6/server.py
@@ -50,20 +50,23 @@ class ClientServerProtocol(asyncio.Protocol):
         resp = self.process_data(data.decode())
         self.transport.write(resp.encode())
 
+def run_server(host, port):
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(
+        ClientServerProtocol,
+        host, port
+    )
 
-loop = asyncio.get_event_loop()
-coro = loop.create_server(
-    ClientServerProtocol,
-    '127.0.0.1', 8181
-)
+    server = loop.run_until_complete(coro)
 
-server = loop.run_until_complete(coro)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+if __name__ == '__main__':
+    run_server('127.0.0.1', 8888)
